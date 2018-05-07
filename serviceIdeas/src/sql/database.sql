654949dd85709f2b1a8d9237e34cfefd7219b430
Jorge Escobar <jorge.escobar@truextend.com>
2018-05-06 20:09:20 -0400

    Ultimas modificaciones ideas

--- serviceIdeas/src/sql/database.sql
+++ serviceIdeas/src/sql/database.sql
@@ -1,18 +1,7 @@
-CREATE DATABASE news_portal;
-
-USE news_portal;
-
-SHOW TABLES;
-
-CREATE TABLE news (
-  id_news INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(100),
-  news TEXT,
-  data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-DESCRIBE news;
-
-INSERT INTO news (title, news) values ('my title', 'content of the news');
-
-SELECT * FROM news;
+CREATE TABLE `ideas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idea` varchar(200) NOT NULL,
+  `proposername` varchar(100) NOT NULL,
+  `votes` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
