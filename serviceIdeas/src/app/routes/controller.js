5717ef5df7c281ed4f0fc99552f0cafd3b612a3e
Carlos David <carlosmontellano@gmail.com>
2018-05-07 13:36:09 -0400

    obtener userid desde eltoken

--- serviceIdeas/src/app/routes/controller.js
+++ serviceIdeas/src/app/routes/controller.js
@@ -7,54 +7,53 @@ var logger = require('../../config/log');
 
 module.exports = app => {
 
-  const connection = dbConnection();
+    const connection = dbConnection();
 
-  app.use(function(idea, req, res, next) {
-    res.status(200).send(idea);
-  });
-
-  app.get('/ideas', VerifyToken, (req, res) => {
-    logger.info("Begin List ideas");
-    connection.query('SELECT * FROM dbideas.ideas ORDER BY id', (err, result) => {
-      res.json(result);
+    app.use(function(idea, req, res, next) {
+        res.status(200).send(idea);
     });
-    logger.info("End List ideas");
-  });
 
-  app.post('/ideas', VerifyToken, (req, res) => {
-    logger.info("Begin Insert ideas");
-    const { idea, proposername } = req.body;
-    var sql = "INSERT INTO dbideas.ideas (idea, proposername, votes) VALUES('"+idea+"', '"+proposername+"', 0);";
+    app.get('/ideas', VerifyToken, (req, res) => {
+        logger.info("Begin List ideas");
+        connection.query('SELECT * FROM dbideas.ideas ORDER BY id', (err, result) => {
+            res.json(result);
+        });
+        logger.info("End List ideas");
+    });
+
+    app.post('/ideas', VerifyToken, (req, res) => {
+        logger.info("Begin Insert ideas");
+
+        const { idea } = req.body;
+        var sql = "INSERT INTO dbideas.ideas (idea, proposername, votes,userid) VALUES('" + idea + "', '" + req.name + "', 0,'" + req.userId + "');";
         console.log(sql);
 
-        connection.query(sql
-        , function(err, result) {
-          if (err) {
-            res.json({ error: err })
-          };
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({ error: err })
+            };
             console.log("Idea inserted");
             logger.info("Idea inserted");
         });
-      res.end();   
-      logger.info("End Insert ideas");
-  });
+        res.end();
+        logger.info("End Insert ideas");
+    });
 
-  app.delete('/ideas', VerifyToken, (req, res) => {
-    logger.info("Begin Delete ideas");
-    const { ideaid } = req.body;
-    
-    var sql = "DELETE FROM dbideas.ideas WHERE id =  "+ideaid;
+    app.delete('/ideas', VerifyToken, (req, res) => {
+        logger.info("Begin Delete ideas");
+        const { ideaid } = req.body;
+
+        var sql = "DELETE FROM dbideas.ideas WHERE id =  " + ideaid;
         console.log(sql);
 
-        connection.query(sql
-        , function(err, result) {
-          if (err) {
-            res.json({ error: err })
-          };
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({ error: err })
+            };
             console.log("Idea Deleted");
             logger.info("Idea Deleted");
         });
-      res.end();   
-      logger.info("End Delete ideas");
-  });
+        res.end();
+        logger.info("End Delete ideas");
+    });
 };
--- serviceIdeas/src/app/routes/controllervotes.js
+++ serviceIdeas/src/app/routes/controllervotes.js
@@ -7,57 +7,55 @@ var logger = require('../../config/log');
 
 module.exports = app => {
 
-  const connection = dbConnection();
+    const connection = dbConnection();
 
-  app.use(function(idea, req, res, next) {
-    res.status(200).send(idea);
-  });
-
-  // confirmar si esto es requerido
-  app.get('/votes', VerifyToken, (req, res) => {
-    logger.info("Begin List votes");
-    connection.query('SELECT * FROM dbideas.votes ORDER BY id', (err, result) => {
-      res.json(result);
+    app.use(function(idea, req, res, next) {
+        res.status(200).send(idea);
     });
-    logger.info("End List votes");
-  });
 
-  app.post('/votes', VerifyToken, (req, res) => {
-    logger.info("Begin Insert vote");
-    const { voterid, ideaid } = req.body;
-    
-    var sql = "INSERT INTO dbideas.votes (voterid, ideaid) VALUES ('"+voterid+"', "+ideaid+");";
+    // confirmar si esto es requerido
+    app.get('/votes', VerifyToken, (req, res) => {
+        logger.info("Begin List votes");
+        connection.query('SELECT * FROM dbideas.votes ORDER BY id', (err, result) => {
+            res.json(result);
+        });
+        logger.info("End List votes");
+    });
+
+    app.post('/votes', VerifyToken, (req, res) => {
+        logger.info("Begin Insert vote");
+        const { ideaid } = req.body;
+
+        var sql = "INSERT INTO dbideas.votes (userid, ideaid) VALUES ('" + req.userId + "', " + ideaid + ");";
         console.log(sql);
 
-        connection.query(sql
-        , function(err, result) {
-          if (err) {
-            res.json({ error: err })
-          };
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({ error: err })
+            };
             console.log("Vote Inserted");
             logger.info("Vote Inserted");
         });
-      res.end();   
-      logger.info("End Insert vote");
-  });
+        res.end();
+        logger.info("End Insert vote");
+    });
 
-  app.delete('/votes', VerifyToken, (req, res) => {
-    logger.info("Begin Delete vote");
+    app.delete('/votes', VerifyToken, (req, res) => {
+        logger.info("Begin Delete vote");
 
-    const { voterid, ideaid } = req.body;
-    
-    var sql = "DELETE FROM dbideas.votes WHERE voterid = '"+voterid+"' OR ideaid = "+ideaid;
+        const { voterid, ideaid } = req.body;
+
+        var sql = "DELETE FROM dbideas.votes WHERE voterid = '" + voterid + "' OR ideaid = " + ideaid;
         console.log(sql);
 
-        connection.query(sql
-        , function(err, result) {
-          if (err) {
-            res.json({ error: err })
-          };
+        connection.query(sql, function(err, result) {
+            if (err) {
+                res.json({ error: err })
+            };
             console.log("Vote Deleted");
             logger.info("Vote Deleted");
         });
-      res.end();   
-      logger.info("End Delete vote");
-  });
+        res.end();
+        logger.info("End Delete vote");
+    });
 };
