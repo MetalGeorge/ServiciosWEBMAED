6591f80b43c5ab51e84171c6afa754fa6eec1e0e
Carlos David <carlosmontellano@gmail.com>
2018-05-08 00:23:36 -0400

    actualiza estadisticas

--- serviceStats/src/models/Stat.js
+++ serviceStats/src/models/Stat.js
@@ -3,17 +3,22 @@ const dbConnection = require('../config/dbConnection');
 
 var Stat = {
 
-    updatestatbyproposer: function(callback) {
-        return dbConnection.query("SELECT * FROM votes", callback);
-    },
-    updatestatbyidea: function(ideaid, callback) {
+    updatestatbyproposer: function(proposerid) {
         const connection = dbConnection();
-        connection.query('SELECT * FROM dbideas.votes ORDER BY id', (err, result) => {
-            // res.json(result);
+        sql = "update ideas set votes=(select count(*) from votes'";
+        sql = sql + " where votes.ideaid=ideas.id) where ideaid in ";
+        sql = sql + " (SELECT distinct ideaid FROM votes where userid='" + proposeid + "')";
+        connection.query(sql, (err, result) => {
+            console.log("actualizo con exito");
         });
-        return {};
+    },
+    updatestatbyidea: function(ideaid) {
+        const connection = dbConnection();
+        sql = "update ideas set votes=(select count(*) from votes where votes.ideaid=ideas.id) where id=" + ideaid;
+        connection.query(sql, (err, result) => {
+            console.log("actualizo con exito");
+        });
 
-        //dbConnection.query("SELECT * FROM ideas where id=" + ideaid, callback);
     },
 
 
